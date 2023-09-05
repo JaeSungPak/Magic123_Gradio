57ca93266b6d888a2251c243c99e31494dda3ad4
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-09-05 17:49:21 +0900

    Update magic_gradio.py

--- magic_gradio.py
+++ magic_gradio.py
@@ -10,7 +10,7 @@ def generate_mesh(input_image):
     input_path = "./input"
     output_path = "./out"
     image_path = input_path + "/input.png"
-    GPU_NUM = "1"
+    GPU_NUM = "0"
 
     if os.path.exists(input_path):
       shutil.rmtree(input_path)
@@ -32,12 +32,12 @@ def generate_mesh(input_image):
         print(e.stdout)
         print(e.stderr)
 
-    output_name = f"./out/magic123-nerf-dmtet/magic123_input_nerf_dmtet/mesh/mesh.obj"
+    output_name = f"./out/magic123-nerf-dmtet/magic123_input_nerf_dmtet/mesh/mesh.glb"
     return output_name
 
-image = Image.open("./0.png")
-generate_mesh(image)
+#image = Image.open("./0.png")
+#generate_mesh(image)
 
-#inputs = gr.inputs.Image(label="Image", type="pil")
-#outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
-#gr.Interface(generate_mesh, inputs, outputs).launch()
+inputs = gr.inputs.Image(label="Image", type="pil")
+outputs = gr.Model3D(label="3D Mesh", clear_color=[1.0, 1.0, 1.0, 1.0])
+gr.Interface(generate_mesh, inputs, outputs).launch()
