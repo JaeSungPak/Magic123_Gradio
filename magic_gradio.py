3b88adb059096d60d62da91163e89b1f32c26e73
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2023-09-12 18:50:43 +0900

    update

--- magic_gradio.py
+++ magic_gradio.py
@@ -7,8 +7,8 @@ import numpy as np
 import shutil
 import time
 import tqdm
-import main_gradio as coarse
-import main_gradio as fine
+import main_coarse
+import main_fine
 
 with gr.Blocks() as demo:
     
@@ -51,9 +51,9 @@ with gr.Blocks() as demo:
                 time.sleep(0.01)
                     
             #Coarse Stage
-            coarse.run(dmtet=False, iters=epoch)
+            main_coarse.run(dmtet=False, iters=epoch)
             #Fine Stage
-            fine.run(dmtet=True, iters=epoch)
+            main_fine.run(dmtet=True, iters=epoch)
             
         except subprocess.CalledProcessError as e:
             print(f"Error occurred: {e}")
--- main_coarse.py
+++ main_coarse.py
@@ -11,7 +11,6 @@ import yaml
 from easydict import EasyDict as edict
 import dnnultis
 import logging
-import importlib
 
 logger = logging.getLogger(__name__)
 
@@ -396,8 +395,6 @@ def init_opt(dmtet, iters=500):
 
 
 def run(dmtet=True, iters=500):
-    
-    importlib.reload(nerf.utils)
     args, args_text = _parse_args()
     opt = init_opt(dmtet,iters=iters)
     
